8f0210c40c2f8138ea8ae4d250cedfc5ad6c51e9
Jungbin <kori1215@gmail.com>
2020-04-08 20:46:15 +0900

    myticket

--- SmartGateAndroid/app/src/main/java/com/chambit/smartgate/ui/main/myticket/MyTicketActivity.kt
+++ SmartGateAndroid/app/src/main/java/com/chambit/smartgate/ui/main/myticket/MyTicketActivity.kt
@@ -3,11 +3,15 @@ package com.chambit.smartgate.ui.main.myticket
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.chambit.smartgate.R
+import com.chambit.smartgate.network.FBTicket
+import com.chambit.smartgate.util.Logg
 
 class MyTicketActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_ticket)
+        Logg.d("ssmm11 myticketactivity =")
+        FBTicket().getMyTickets(this)
     }
 }
