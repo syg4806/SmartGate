166cc19166699c4fae3b921db34585ac88eef803
Jungbin <kori1215@gmail.com>
2020-05-17 17:10:05 +0900

    카드 추가 액티비티 만들었는데 갔다가 와서 할게요 !! 죄송합니다!

---
+++ SmartGateAndroid/app/src/main/java/com/chambit/smartgate/ui/main/mypage/cardmanagement/CardAddActivity.kt
@@ -0,0 +1,12 @@
+package com.chambit.smartgate.ui.main.mypage.cardmanagement
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CardAddActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_card_add)
+  }
+}
